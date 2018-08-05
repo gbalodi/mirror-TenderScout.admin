5ca72d649b84fdc348449bce0d9d24f756d9430b
max <merkulov89@gmail.com>
2018-08-05 09:51:33 +0300

    Polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -35,10 +35,10 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /** Evergreen browsers require these. **/
@@ -66,4 +66,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+import 'intl';  // Run `npm install --save intl`.
