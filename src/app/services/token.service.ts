fc70c7dfccd922ee4872cd1ecd5b8cfe86611019
Merkulov Maxim <merkulovdev@gmail.com>
2018-08-09 17:58:22 +0300

    Minor. JWT module

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,11 +1,9 @@
 import { Injectable, Inject } from '@angular/core';
-import { JwtHelper } from 'angular2-jwt';
 import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable()
 export class TokenService {
 
-    jwtHelper: JwtHelper = new JwtHelper();
 /*
 
     public accessToken = this.localStorage.retrieve('access_token') ? this.jwtHelper.decodeToken(this.localStorage.retrieve('access_token')) : '';
