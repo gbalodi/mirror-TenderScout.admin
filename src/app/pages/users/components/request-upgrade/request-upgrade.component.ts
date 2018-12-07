69900d7ba471bd7559cf6dadbb8f003176460659
Merkulov Maxim <merkulovdev@gmail.com>
2018-12-07 17:28:39 +0200

    User. Upgrade account type

---
+++ src/app/pages/users/components/request-upgrade/request-upgrade.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {MainRequestService} from '../../../../services/main-request.service';
+import {ToastrService} from 'ngx-toastr';
+import {DatePipe} from '@angular/common';
+import {StatusSwitcherComponent} from '../signup-request-list/status-switcher/status-switcher.component';
+import {UserInfoComponent} from './user-info/user-info.component';
+
+@Component({
+  selector: 'app-request-assistance',
+  templateUrl: './request-upgrade.component.html',
+  styleUrls: ['./request-upgrade.component.scss']
+})
+export class RequestUpgradeComponent implements OnInit {
+
+
+    constructor(
+        private request: MainRequestService,
+        private toasterService: ToastrService,
+        private datePipe: DatePipe,
+    ) { }
+
+    public requestsList;
+    public settings = {
+        actions: {
+            delete: false,
+            add: false,
+            edit: false,
+        },
+        columns: {
+            id: {
+                title: 'ID',
+                editable: false,
+            },
+            email: {
+                title: 'Email',
+                editable: false,
+            },
+            created_at: {
+                title: 'Created at',
+                filter: false,
+                sort: false,
+                valuePrepareFunction: (date) => {
+                    if (date) {
+                        const raw = new Date(date);
+
+                        const formatted = this.datePipe.transform(raw, 'dd.MM.yyyy');
+                        return formatted;
+                    }
+                }
+            },
+            profiles: {
+                title: 'Details',
+                type: 'custom',
+                editable: false,
+                filter: false,
+                sort: false,
+                renderComponent: UserInfoComponent,
+            }
+        }
+    };
+
+    ngOnInit() {
+        this.request.getData('v1/users/upgrade_requests' ).subscribe( res => {
+            this.requestsList = [];
+            JSON.parse(res).forEach( item => {
+               item.user['created_at'] = item.created_at;
+               item.user['upgraded_at'] = item.upgraded_at;
+
+               this.requestsList.push(item.user);
+            });
+        })
+    }
+}
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersListComponent } from './components/users-list/users-list.component';
-import { SignupReqListComponent } from './components/signup-request-list/reqs-list.component';
-import { CreateUserComponent } from './components/create-user/create-user.component';
-import { DetailsComponent } from './components/signup-request-list/details/details.component';
 
 // Import the library
 import { TimezonePickerModule } from 'ng2-timezone-selector';
 import { SharedModule } from '../../modules/shared.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { ModalModule } from 'ngx-bootstrap';
-import { StatusSwitcherComponent } from './components/signup-request-list/status-switcher/status-switcher.component';
 import { UiSwitchModule } from 'ngx-toggle-switch';
+
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { SignupReqListComponent } from './components/signup-request-list/reqs-list.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { DetailsComponent } from './components/signup-request-list/details/details.component';
+import { StatusSwitcherComponent } from './components/signup-request-list/status-switcher/status-switcher.component';
 import { RequestAssistanceComponent } from './components/request-assistance/request-assistance.component';
+import { RequestUpgradeComponent } from './components/request-upgrade/request-upgrade.component';
+import { UserInfoComponent } from './components/request-upgrade/user-info/user-info.component';
+
 
 @NgModule({
     imports: [
@@ -30,12 +34,15 @@ import { RequestAssistanceComponent } from './components/request-assistance/requ
         UsersListComponent,
         DetailsComponent,
         StatusSwitcherComponent,
-        RequestAssistanceComponent
+        RequestAssistanceComponent,
+        RequestUpgradeComponent,
+        UserInfoComponent,
     ],
     providers: [ ],
     entryComponents: [
         DetailsComponent,
-        StatusSwitcherComponent
+        StatusSwitcherComponent,
+        UserInfoComponent,
     ]
 })
 export class UsersModule {
