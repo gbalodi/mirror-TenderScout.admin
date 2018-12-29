a3a50208bd74f46d6a4bc465c06ed8d719a6211f
Merkulov Maxim <merkulovdev@gmail.com>
2018-12-29 12:03:02 +0200

    TEN-472. UserList. Marketplace availability. Hide elements with 0 length

--- src/app/pages/users/components/signup-request-list/details/details.component.ts
+++ src/app/pages/users/components/signup-request-list/details/details.component.ts
@@ -22,10 +22,10 @@ export class DetailsComponent implements ViewCell, OnInit {
     ngOnInit() {
         if(this.rowData && this.rowData.onlyProfile){
             for(let elem in this.rowData.profile){
-                if(this.rowData.profile[elem] == null || !this.rowData.profile[elem].length || this.rowData.profile[elem] == undefined) {
+                if(this.rowData.profile[elem] === null || this.rowData.profile[elem].length == 0 || this.rowData.profile[elem] === undefined) {
                     delete this.rowData.profile[elem];
                 }
-               /* if(typeof this.rowData.profile[elem] === 'object' && this.rowData.profile[elem] !== null) {
+                /*if(typeof this.rowData.profile[elem] === 'object' && this.rowData.profile[elem] !== null) {
                     console.log(this.rowData.profile[elem]);//TODO: Delete
                 }*/
             }
