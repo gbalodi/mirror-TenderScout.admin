e91bc3fb1dd9c4c528b80153aef89b4ad5916c63
magankur <ankur@matrixmarketers.com>
2019-07-19 12:18:30 +0530

    Updated code models are setted on documents-list 19 July 12:18

--- src/app/pages/documents/components/documents-list/documents-list.component.ts
+++ src/app/pages/documents/components/documents-list/documents-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-documents-list',
   templateUrl: './documents-list.component.html',
-  styleUrls: ['./documents-list.component.scss']
+  styleUrls: ['./documents-list.component.scss'],
+  providers: [
+    BsModalService
+  ]
 })
 export class DocumentsListComponent implements OnInit {
+  public bsModalRef: BsModalRef;
 
-  constructor() { }
+  constructor(
+    private bsModalService: BsModalService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public openModal(template: TemplateRef<any>) {
+    this.bsModalRef = this.bsModalService.show(template);
   }
+
 
 }
