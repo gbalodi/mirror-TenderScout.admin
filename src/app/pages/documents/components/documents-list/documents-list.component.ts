92c763304643fee82d3a8d9f4c2bdd9d531c179b
magankur <ankur@matrixmarketers.com>
2019-07-19 13:12:17 +0530

    Updated code 19 July 13:12

--- src/app/pages/documents/components/documents-list/documents-list.component.ts
+++ src/app/pages/documents/components/documents-list/documents-list.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-documents-list',
   templateUrl: './documents-list.component.html',
-  styleUrls: ['./documents-list.component.scss'],
-  providers: [
-    BsModalService
-  ]
+  styleUrls: ['./documents-list.component.scss']
 })
 export class DocumentsListComponent implements OnInit {
   public bsModalRef: BsModalRef;
